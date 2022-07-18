ef26f1eaf9fd2a46e588fad60d65ab9c569c5979
mafeZapata <mafez0427@gmail.com>
2022-07-18 10:55:13 -0500

    Testing with Page object model (POM)

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -1,28 +1,36 @@
+import {
+  AddressStepPage,
+  LoginPage,
+  MenuContentPage,
+  PaymentStepPage,
+  ProductsListPage,
+  ShippingStepPage,
+  ShopingCartPage,
+} from "../page/index";
+
+const menuContentPage = new MenuContentPage();
+const productsListPage = new ProductsListPage();
+const shopingCartPage = new ShopingCartPage();
+const loginPage = new LoginPage();
+const addressStepPage = new AddressStepPage();
+const shippingStepPage = new ShippingStepPage();
+const paymentStepPage = new PaymentStepPage();
+
+
 describe("Buy a t-shirt", () => {
-  it("then the t-shirt should be bought", () => {
-    cy.visit("http://automationpractice.com/");
+  it("then should be bought a t-shirt", () => {
+    menuContentPage.visitMenuContentPage();
+    menuContentPage.goToTShirtMenu();
+    productsListPage.addTShirtToCart();
+    shopingCartPage.goToCartPage();
+    shopingCartPage.goToCheckoutPage();
+    loginPage.login("aperdomobo@gmail.com", "WorkshopProtractor");
+    addressStepPage.goToShippingStep();
+    shippingStepPage.acceptTermsAndConditions();
+    shippingStepPage.goToPaymentStep();
+    paymentStepPage.selectPaymentMethod();
+    paymentStepPage.confirmOrder();
 
-    cy.get("#block_top_menu > ul > li:nth-child(3) > a").click();
-
-    cy.get(".ajax_add_to_cart_button > span").click();
-    cy.get(".layer_cart_cart > .button-container").click();
-
-    cy.get(".cart_navigation span").click();
-
-    cy.get("#email").type("aperdomobo@gmail.com");
-    cy.get("#passwd").type("WorkshopProtractor");
-    cy.get("#SubmitLogin > span").click();
-
-    cy.get(".cart_navigation > .button").click();
-
-    cy.get("#cgv").check();
-
-    cy.get(".cart_navigation > .button").click();
-
-    cy.get(".bankwire").click();
-    cy.get("#cart_navigation > .button").click();
-
-    cy.get("#center_column > div > p > strong")
-        .should("have.text", "Your order on My Store is complete.");
+    paymentStepPage.getOrderLabel().should("have.text", "Your order on My Store is complete."); ;
   });
 });

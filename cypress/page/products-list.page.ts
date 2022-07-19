f10a71e2f989b28dd8710399047e9c02c4e4ac99
mafeZapata <mafez0427@gmail.com>
2022-07-19 10:49:53 -0500

    Update products-list.page.ts

--- cypress/page/products-list.page.ts
+++ cypress/page/products-list.page.ts
@@ -1,8 +1,7 @@
 class ProductsListPage {
     private addToCartBtn: string;
-  
     constructor() {
-        this.addToCartBtn = "#center_column a.button.ajax_add_to_cart_button.btn.btn-default";
+        this.addToCartBtn = ".ajax_add_to_cart_button";
     }
 
     public addTShirtToCart(): void {

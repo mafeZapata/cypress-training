7173ac2606037ebdc841cd77720c47c17cb0a581
mafeZapata <mafez0427@gmail.com>
2022-07-19 16:47:56 -0500

    Update buy-shirt.cy.ts

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -11,44 +11,38 @@ import {
 
 
 describe("Buy a t-shirt", () => {
-
-
-      let addressStepPage: AddressStepPage;
-      let loginPage: LoginPage;
-      let menuContentPage: MenuContentPage;
-      let paymentStepPage: PaymentStepPage;
-      let productsListPage: ProductsListPage;
-      let shippingStepPage: ShippingStepPage;
-      let shoppingCartPage: ShoppingCartPage;
+  let addressStepPage: AddressStepPage;
+  let loginPage: LoginPage;
+  let menuContentPage: MenuContentPage;
+  let paymentStepPage: PaymentStepPage;
+  let productsListPage: ProductsListPage;
+  let shippingStepPage: ShippingStepPage;
+  let shoppingCartPage: ShoppingCartPage;
 
   before(()=> {
-
-        menuContentPage = new MenuContentPage();
-        productsListPage = new ProductsListPage();
-        shoppingCartPage = new ShoppingCartPage();
-        loginPage = new LoginPage();
-        addressStepPage = new AddressStepPage();
-        shippingStepPage = new ShippingStepPage();
-        paymentStepPage = new PaymentStepPage();
-        menuContentPage.visitMenuContentPage();
-
-    });
+    menuContentPage = new MenuContentPage();
+    productsListPage = new ProductsListPage();
+    shoppingCartPage = new ShoppingCartPage();
+    loginPage = new LoginPage();
+    addressStepPage = new AddressStepPage();
+    shippingStepPage = new ShippingStepPage();
+    paymentStepPage = new PaymentStepPage();
+    menuContentPage.visitMenuContentPage();
+  });
 
   it("try to buy a T-shirt", () => {
-
-        menuContentPage.visitMenuContentPage();
-        menuContentPage.goToTShirtMenu();
-        productsListPage.addTShirtToCart();
-        productsListPage.goToCheckPage();
-        shoppingCartPage.goToCheckoutPage();
-        loginPage.login("aperdomobo@gmail.com", "WorkshopProtractor");
-        addressStepPage.goToShippingStep();
-        shippingStepPage.acceptTermsAndConditions();
-        shippingStepPage.goToPaymentStep();
-        paymentStepPage.selectPaymentMethod();
-        paymentStepPage.confirmOrder();
-        paymentStepPage.getOrderLabel().should("have.text", "Your order on My Store is complete.");
-
-    });
+    menuContentPage.visitMenuContentPage();
+    menuContentPage.goToTShirtMenu();
+    productsListPage.addTShirtToCart();
+    productsListPage.goToCheckPage();
+    shoppingCartPage.goToCheckoutPage();
+    loginPage.login("aperdomobo@gmail.com", "WorkshopProtractor");
+    addressStepPage.goToShippingStep();
+    shippingStepPage.acceptTermsAndConditions();
+    shippingStepPage.goToPaymentStep();
+    paymentStepPage.selectPaymentMethod();
+    paymentStepPage.confirmOrder();
+    paymentStepPage.getOrderLabel().should("have.text", "Your order on My Store is complete.");
   });
+});
 

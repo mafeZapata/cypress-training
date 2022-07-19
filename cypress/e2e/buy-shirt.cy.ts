404494d4f1a2cbcb9995d65b19688e3c91c326a1
mafeZapata <mafez0427@gmail.com>
2022-07-19 16:42:53 -0500

    Using AAA pattern

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -9,28 +9,46 @@ import {
   ShoppingCartPage,
 } from "../page/index";
 
-const menuContentPage = new MenuContentPage();
-const productsListPage = new ProductsListPage();
-const loginPage = new LoginPage();
-const addressStepPage = new AddressStepPage();
-const shippingStepPage = new ShippingStepPage();
-const paymentStepPage = new PaymentStepPage();
-const shoppingCartPage = new ShoppingCartPage();
-
 
 describe("Buy a t-shirt", () => {
-  it("then should be bought a t-shirt", () => {
-    menuContentPage.visitMenuContentPage();
-    menuContentPage.goToTShirtMenu();
-    productsListPage.addTShirtToCart();
-    productsListPage.goToCheckPage();
-    shoppingCartPage.goToCheckoutPage();
-    loginPage.login("aperdomobo@gmail.com", "WorkshopProtractor");
-    addressStepPage.goToShippingStep();
-    shippingStepPage.acceptTermsAndConditions();
-    shippingStepPage.goToPaymentStep();
-    paymentStepPage.selectPaymentMethod();
-    paymentStepPage.confirmOrder();
-    paymentStepPage.getOrderLabel().should("have.text", "Your order on My Store is complete."); ;
+
+
+      let addressStepPage: AddressStepPage;
+      let loginPage: LoginPage;
+      let menuContentPage: MenuContentPage;
+      let paymentStepPage: PaymentStepPage;
+      let productsListPage: ProductsListPage;
+      let shippingStepPage: ShippingStepPage;
+      let shoppingCartPage: ShoppingCartPage;
+
+  before(()=> {
+
+        menuContentPage = new MenuContentPage();
+        productsListPage = new ProductsListPage();
+        shoppingCartPage = new ShoppingCartPage();
+        loginPage = new LoginPage();
+        addressStepPage = new AddressStepPage();
+        shippingStepPage = new ShippingStepPage();
+        paymentStepPage = new PaymentStepPage();
+        menuContentPage.visitMenuContentPage();
+
+    });
+
+  it("try to buy a T-shirt", () => {
+
+        menuContentPage.visitMenuContentPage();
+        menuContentPage.goToTShirtMenu();
+        productsListPage.addTShirtToCart();
+        productsListPage.goToCheckPage();
+        shoppingCartPage.goToCheckoutPage();
+        loginPage.login("aperdomobo@gmail.com", "WorkshopProtractor");
+        addressStepPage.goToShippingStep();
+        shippingStepPage.acceptTermsAndConditions();
+        shippingStepPage.goToPaymentStep();
+        paymentStepPage.selectPaymentMethod();
+        paymentStepPage.confirmOrder();
+        paymentStepPage.getOrderLabel().should("have.text", "Your order on My Store is complete.");
+
+    });
   });
-});
+
--- cypress/page/menu-content.page.ts
+++ cypress/page/menu-content.page.ts
@@ -1,11 +1,12 @@
 class MenuContentPage {
     private tShirtMenu: string;
     private menuContentPageURL: string
+    private dressesMenu: string;
 
     constructor() {
-    
         this.tShirtMenu = "#block_top_menu > ul > li > a[title='T-shirts']";
-        this.menuContentPageURL = "http://automationpractice.com/"
+        this.dressesMenu = "#block_top_menu > .sf-menu > li > a[title='Dresses']";
+        this.menuContentPageURL = "http://automationpractice.com/";
     }
 
     public visitMenuContentPage(): void {
@@ -14,6 +15,10 @@ class MenuContentPage {
 
     public goToTShirtMenu(): void {
         cy.get(this.tShirtMenu).click()
+    }
+
+    public goToDressesMenu(): void{
+        cy.get(this.dressesMenu).click()
     }
 }
 

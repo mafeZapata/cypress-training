00e89f004897e794bd6d34863eeb2b3c1d4fe865
mafeZapata <49245929+mafeZapata@users.noreply.github.com>
2022-07-19 16:48:16 -0500

    Improving selectors (#9)
    * Preparing  environment for CI
    
    * Update main.yml
    
    * Updated Preparing environment for CI
    
    * Update main.yml
    
    * Updated Preparing environment for CI
    
    * Update .nvmrc
    
    * Update package-lock.json
    
    * Selectors css
    
    * Update buy-shirt.cy.ts
    
    * Update buy-shirt.cy.ts
    
    * Testing with Page object model (POM)
    
    * Update buy-shirt.cy.ts
    
    * Changes in the type
    
    * Improving selectors
    
    * Update improving selectors
    
    * Update products-list.page.ts
    
    * Update improving selectors
    
    * corrections
    
    * Update
    
    * Delete shopping-cart.page.ts
    
    * Update shopping-cart-page.ts

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -9,21 +9,32 @@ import {
   ShoppingCartPage,
 } from "../page/index";
 
-const menuContentPage = new MenuContentPage();
-const productsListPage = new ProductsListPage();
-const shoppingCartPage = new ShoppingCartPage();
-const loginPage = new LoginPage();
-const addressStepPage = new AddressStepPage();
-const shippingStepPage = new ShippingStepPage();
-const paymentStepPage = new PaymentStepPage();
-
 
 describe("Buy a t-shirt", () => {
-  it("then should be bought a t-shirt", () => {
+  let addressStepPage: AddressStepPage;
+  let loginPage: LoginPage;
+  let menuContentPage: MenuContentPage;
+  let paymentStepPage: PaymentStepPage;
+  let productsListPage: ProductsListPage;
+  let shippingStepPage: ShippingStepPage;
+  let shoppingCartPage: ShoppingCartPage;
+
+  before(()=> {
+    menuContentPage = new MenuContentPage();
+    productsListPage = new ProductsListPage();
+    shoppingCartPage = new ShoppingCartPage();
+    loginPage = new LoginPage();
+    addressStepPage = new AddressStepPage();
+    shippingStepPage = new ShippingStepPage();
+    paymentStepPage = new PaymentStepPage();
+    menuContentPage.visitMenuContentPage();
+  });
+
+  it("try to buy a T-shirt", () => {
     menuContentPage.visitMenuContentPage();
     menuContentPage.goToTShirtMenu();
     productsListPage.addTShirtToCart();
-    shoppingCartPage.goToCartPage();
+    productsListPage.goToCheckPage();
     shoppingCartPage.goToCheckoutPage();
     loginPage.login("aperdomobo@gmail.com", "WorkshopProtractor");
     addressStepPage.goToShippingStep();
@@ -31,7 +42,7 @@ describe("Buy a t-shirt", () => {
     shippingStepPage.goToPaymentStep();
     paymentStepPage.selectPaymentMethod();
     paymentStepPage.confirmOrder();
-
-    paymentStepPage.getOrderLabel().should("have.text", "Your order on My Store is complete."); ;
+    paymentStepPage.getOrderLabel().should("have.text", "Your order on My Store is complete.");
   });
 });
+
--- cypress/page/menu-content.page.ts
+++ cypress/page/menu-content.page.ts
@@ -1,10 +1,12 @@
 class MenuContentPage {
     private tShirtMenu: string;
     private menuContentPageURL: string
+    private dressesMenu: string;
 
     constructor() {
-        this.tShirtMenu = "#block_top_menu > ul > li:nth-child(3) > a";
-        this.menuContentPageURL = "http://automationpractice.com/"
+        this.tShirtMenu = "#block_top_menu > ul > li > a[title='T-shirts']";
+        this.dressesMenu = "#block_top_menu > .sf-menu > li > a[title='Dresses']";
+        this.menuContentPageURL = "http://automationpractice.com/";
     }
 
     public visitMenuContentPage(): void {
@@ -13,6 +15,10 @@ class MenuContentPage {
 
     public goToTShirtMenu(): void {
         cy.get(this.tShirtMenu).click()
+    }
+
+    public goToDressesMenu(): void{
+        cy.get(this.dressesMenu).click()
     }
 }
 
